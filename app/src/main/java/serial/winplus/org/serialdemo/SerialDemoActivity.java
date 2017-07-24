4f690a75914125731de6f02362e7d4c65ca56410
zpjgit <zpj.love@foxmail.com>
2017-07-24 17:34:11 +0800

    刚刚创建 空项目

---
+++ app/src/main/java/serial/winplus/org/serialdemo/SerialDemoActivity.java
@@ -0,0 +1,13 @@
+package serial.winplus.org.serialdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SerialDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_serial_demo);
+    }
+}
